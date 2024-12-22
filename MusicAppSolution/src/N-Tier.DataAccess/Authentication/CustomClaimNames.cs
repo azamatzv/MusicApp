2f6cce7e0830410f5a321be50aa54fa5531f8f9f
Muhammad <abdurashidovmuhammad915@gmail.com>
2024-12-22 14:08:34 +0500

    add some

---
+++ MusicAppSolution/src/N-Tier.DataAccess/Authentication/CustomClaimNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_Tier.DataAccess.Authentication;
+
+public class CustomClaimNames
+{
+    public const string Email = "email";
+    public const string Role = "role";
+    public const string Id = "id";
+}
---
+++ MusicAppSolution/src/N-Tier.DataAccess/Authentication/IJwtTokenHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace N_Tier.DataAccess.Authentication;
+
+public interface IJwtTokenHandler
+{
+    JwtSecurityToken GenerateAccessToken(Users user);
+    string GenerateRefreshToken();
+}
